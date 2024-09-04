3facac5ab3ca39bc4a78036f7caa6f0e25317a43
Mamourou SANGARE <ms284034@iut21.u-bourgogne.fr>
2024-09-04 18:00:33 +0200

    Ajout des bouton de changement de page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,16 @@ type Post = {
 	created_at: string;
 }
 
-async function requet() {
-	
+async function requet(page:number) {
+	const urlParameters = new URLSearchParams({
+		page: (page ?? 1).toString(),
+		per_page: '6',
+		orderBy: 'date',
+	});
 	try {
 		// J'attend la fin de mon fetch et stoque le résultat en variable
-		const result = await fetch("https://dev.to/api/articles");
+		const result = await fetch("https://dev.to/api/articles?"+urlParameters);
 		const decodedJSON = await result.json() as Post[] ;
-		console.log(decodedJSON);
 		return decodedJSON;
     // Si mon fetch est OK, je continue ici
 	} 
@@ -28,10 +31,22 @@ async function requet() {
 }
 
 
-export default async function Home() {
+export default async function Home(props : object) {
 	
-  const  articles = await requet(); 
-  
+	//console.log(props) /?page=${numPage+1}
+	// faire une variable qui récupère le numéro de page stocké dans l'URL actuelle
+	// @ts-ignore
+	let numPage = props['searchParams']['page'];
+	
+	numPage = parseInt(numPage ?? '1');
+
+	
+	
+	console.log(props);
+	const  articles = await requet(numPage); 
+	console.log(numPage);
+
+
   return (<main ><pre> 
 	
 	</pre>
@@ -42,7 +57,7 @@ export default async function Home() {
 	 <div className = "grid grid-cols-3 gap-5" >
 		
 		{articles.map((post) => 
-		<div className = "size-62 bg-blue-400 rounded-lg "  >
+		<div key={post.id} className = "size-62 bg-blue-400 rounded-lg "  >
 			<img src={post.cover_image} className="rounded-lg" />
 			
 			<p><b>{post.title}</b></p>
@@ -55,10 +70,17 @@ export default async function Home() {
 		</div>
 	)}
 	</div>
+	<div className= "content-center"  >
 
-	<button className> Page suivante </button>
-
+		<a href= {`http://localhost:3000/?page=${numPage-1}`} className= "absolute h-25 w-20 px-40 m-2 text-lg text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800 left-0 ">Page précédente</a>
+		<a href= {`http://localhost:3000/?page=${numPage+1}`} className= "absolute h-25 w-20 px-40 m-2 text-lg text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800 right-0" > Page suivante </a>
+		
+	
+	</div>
 </main>);
 }
+
+
+
 
 
